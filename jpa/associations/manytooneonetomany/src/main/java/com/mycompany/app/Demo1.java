024645b6bccc337214cbd73ff196a9821d9c0b2b
vineetsemwal <vineetsemwal82@gmail.com>
2021-11-15 17:49:03 +0530

    manytoone one to many bi demo added

---
+++ jpa/associations/manytooneonetomany/src/main/java/com/mycompany/app/Demo1.java
@@ -0,0 +1,80 @@
+package com.mycompany.app;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Demo1 {
+
+    public static void main(String arg[]) {
+        Demo1 demo = new Demo1();
+        demo.start();
+    }
+
+
+    public void start() {
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory("demo");
+        EntityManager em = factory.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+
+        Department dev=new Department();
+        dev.setDeptName("development");
+
+
+        Employee emp1 = new Employee();
+        emp1.setName("kaushik");
+        emp1.setSalary(90000);
+
+        Employee emp2 = new Employee();
+        emp2.setName("aisha");
+        emp2.setSalary(80000);
+
+        transaction.begin();
+
+        em.persist(dev);
+
+        emp1.setDepartment(dev);
+        emp2.setDepartment(dev);
+
+
+
+        em.persist(emp1);
+        em.persist(emp2);
+
+
+        Set<Employee> employees=dev.getEmployees();
+        if(employees==null){
+            employees=new HashSet<>();
+            dev.setEmployees(employees);
+        }
+        employees.add(emp1);
+        employees.add(emp2);
+
+        System.out.println("fetching employee and its dept");
+        Employee foundEmp=em.find(Employee.class,emp1.getId());
+        display(foundEmp);
+        Department foundDept=foundEmp.getDepartment();
+        display(foundDept);
+
+        System.out.println("fetch department and its employees");
+        Department searchDept=em.find(Department.class,dev.getId());
+        display(searchDept);
+        System.out.println("Employees of the department");
+        Set<Employee>searchDeptEmployees=searchDept.getEmployees();
+        for(Employee emp:searchDeptEmployees){
+            display(emp);
+        }
+
+        transaction.commit();
+        em.close();
+    }
+
+    void display(Employee emp) {
+        System.out.println("id=" + emp.getId() + " name=" + emp.getName() + " sal=" + emp.getSalary());
+    }
+
+    void display(Department dept) {
+        System.out.println("department-"+dept.getId()+"-"+dept.getDeptName());
+    }
+
+}
