d9091f71bfb9110f2166b67e04738cf26539695c
vineetsemwal <vineetsemwal82@gmail.com>
2021-11-15 18:04:14 +0530

    one to many single directional

---
+++ jpa/associations/onetomanysingle/src/main/java/com/mycompany/app/Department.java
@@ -0,0 +1,58 @@
+package com.mycompany.app;
+
+
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.Set;
+
+
+@Table(name = "depts_data")
+@Entity
+public class Department {
+
+    @GeneratedValue
+    @Id
+    private long id;
+
+    private String deptName;
+
+    @OneToMany
+    private Set<Employee> employees;
+
+    public Set<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(Set<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Department that = (Department) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
