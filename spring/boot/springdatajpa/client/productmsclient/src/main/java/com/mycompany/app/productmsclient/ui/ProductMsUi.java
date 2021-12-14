351cbd3b3c29edc62edc5eb31b8b242a1ed4dcf6
vineetsemwal <vineetsemwal82@gmail.com>
2021-12-14 11:07:01 +0530

    more api examples added in rest demo

--- spring/boot/springdatajpa/client/productmsclient/src/main/java/com/mycompany/app/productmsclient/ui/ProductMsUi.java
+++ spring/boot/springdatajpa/client/productmsclient/src/main/java/com/mycompany/app/productmsclient/ui/ProductMsUi.java
@@ -3,8 +3,16 @@ package com.mycompany.app.productmsclient.ui;
 import com.mycompany.app.productmsclient.dto.CreateProductRequest;
 import com.mycompany.app.productmsclient.dto.ProductResponseDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Component
 public class ProductMsUi {
@@ -16,14 +24,14 @@ public class ProductMsUi {
     public void start() {
         System.out.println("Adding a product");
         CreateProductRequest request=new CreateProductRequest();
-        request.setName("samsung");
+        request.setName("lg");
         request.setPrice(10000);
-        ProductResponseDetails addResponse=addProduct(request);
+        ProductResponseDetails addResponse=addProductByExchange(request);
         System.out.println("product added, response received");
         display(addResponse);
         long productId=addResponse.getId();
         System.out.println("get product by id");
-        ProductResponseDetails getResponse=getProductById(productId);
+        ProductResponseDetails getResponse=getForEntityProductById(productId);
          display(getResponse);
     }
 
@@ -33,11 +41,47 @@ public class ProductMsUi {
         return details;
     }
 
+    public ProductResponseDetails getForEntityProductById(long productId) {
+        String url = baseUrl + "/products/byid/" + productId;
+        ResponseEntity<ProductResponseDetails> responseEntity = restTemplate.getForEntity(url, ProductResponseDetails.class);
+        ProductResponseDetails details=responseEntity.getBody();
+        HttpStatus status=responseEntity.getStatusCode();
+        System.out.println("response status="+status.name()+"http errno="+status.value());
+        return details;
+    }
+
+
     public ProductResponseDetails addProduct(CreateProductRequest requestData) {
         String url = baseUrl + "/products/add";
         ProductResponseDetails details = restTemplate.postForObject(url, requestData, ProductResponseDetails.class);
         return details;
     }
+
+    public ProductResponseDetails addProductByExchange(CreateProductRequest requestData) {
+        String url = baseUrl + "/products/add";
+        HttpEntity requestEntity=new HttpEntity(requestData);
+        Map<String,Object> parameters=new HashMap<>();
+        ResponseEntity<ProductResponseDetails> responseEntity = restTemplate.exchange(url, HttpMethod.POST,requestEntity,ProductResponseDetails.class,parameters);
+        ProductResponseDetails response=responseEntity.getBody();
+        HttpStatus status=responseEntity.getStatusCode();
+        System.out.println("response status="+status.name()+"http errno="+status.value());
+        return response;
+    }
+
+    public ProductResponseDetails addProductByPostForEntity(CreateProductRequest requestData) {
+        String url = baseUrl + "/products/add";
+        HttpEntity requestEntity=new HttpEntity(requestData);
+        ResponseEntity<ProductResponseDetails> responseEntity = restTemplate.postForEntity(url, requestEntity,ProductResponseDetails.class);
+        ProductResponseDetails response=responseEntity.getBody();
+        HttpStatus status=responseEntity.getStatusCode();
+        System.out.println("response status="+status.name()+"http errno="+status.value());
+
+        return response;
+    }
+
+
+
+
 
     public void display(ProductResponseDetails details){
         System.out.println("product-"+details.getId()+"-"+details.getProductName()+"-"+details.getPrice());
